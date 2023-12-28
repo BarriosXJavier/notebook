63be480d942c51e550ee737e088dd06ce78a95f2
David Muriithi <muriithid05@gmail.com>
2023-12-28 16:46:58 +0300

    new file:   components.json 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/App.css 	modified:   src/App.tsx 	new file:   src/components/ui/button.tsx 	modified:   src/index.css 	new file:   src/lib/utils.ts 	modified:   tailwind.config.js 	modified:   tsconfig.json 	modified:   vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
 function App() {
+  // notes from local storage on component mount
+
+  useEffect(() => {
+    const storedNotes = JSON.parse(localStorage.getItem("notes") || "[]");
+    setNotes(storedNotes);
+  }, []);
+
   const [notes, setNotes] = useState([]);
   const [currentNote, setCurrentNote] = useState("");
 
   const addNote = () => {
     if (currentNote.trim()) {
-      setNotes([...notes, currentNote]);
+      const newNotes = [...notes, currentNote];
+      setNotes(newNotes);
+      localStorage.setItem("notes", JSON.stringify(newNotes));
       setCurrentNote("");
     }
   };
@@ -16,29 +25,45 @@ function App() {
     const newNotes = [...notes];
     newNotes.splice(index, 1);
     setNotes(newNotes);
+    localStorage.setItem("notes", JSON.stringify(newNotes));
   };
 
   return (
-    <div className="notebook relative bg-white">
-<input
-type="text"
-value={currentNote}
-onChange={(e) => setCurrentNote(e.target.value)}
-className="border-b border-gray-300 py-2 w-full px-3 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200"
-/>
-<button onClick={addNote} className="bg-blue-500 text-white py-2 px-4 rounded mt-3 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">Add Note</button>
+    <div className="relative h-full]">
+      <input
+        type="text"
+        placeholder="Add note or link"
+        value={currentNote}
+        onChange={(e) => setCurrentNote(e.target.value)}
+        className="border-b border-gray-500 py-2 w-full px-3 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200 mb-1"
+      />
+      <Button
+        onClick={addNote}
+      >
+        Add Note
+      </Button>
 
-<ul className="space-y-2">
-{notes.map((note, index) => (
-<li key={index} className="relative py-4 px-6 bg-gray-100 border-l-4 border-gray-200">
-<span className="block">{note}</span>
-<button onClick={() => removeNote(index)} className="absolute top-2 right-2 bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300">Remove</button>
-</li>
-))}
-</ul>
-<div className="absolute top-0 left-0 h-full border-l-4 border-gray-200"></div>
-</div>
-);
+      <ul className="space-y-2">
+        {notes.map((note, index) => (
+          <li key={index} className="relative py-2 px-6 border-l-4">
+            <span
+              className="block break-all]"
+              style={{ whiteSpace: "pre-wrap" }}
+            >
+              {note}
+            </span>
+            <Button
+              variant="destructive"
+              onClick={() => removeNote(index)}
+              className="absolute top-2 right-2"
+            >
+              Remove
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
